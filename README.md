e16770fd35167d2d6196d46c32b9ca440a6329fc
Chris Brackert <cbrackert@gmail.com>
2016-06-22 16:00:02 -0700

    TextClassifier classifies signals against a training set

--- README.md
+++ README.md
@@ -3,28 +3,24 @@ TextClassifier
 
 Take input text and classify it.
 
+When the block first is configuring, it loads in some training data to build a classifier.
+
 Properties
 ----------
-None
+
+* Training Data - Map text to category
 
 Dependencies
 ------------
 scikit-learn
+scipy
+numpy
 
 Commands
 --------
 None
 
-Input
------
-
-Train
-    Training data is used to define a fit, mapping text to a category (i.e. target value).
-
-Sample
-    Sample data is matched againt the training data to predict a target value.
-
 Output
 ------
 
-Each `Sample` input will output a signal with a new attribute `target` that is the predicted target value, based on the trainging data.
+Each input will output a signal with a new attribute `target` that is the predicted target value, based on the trainging data.
--- tests/test_text_classifier_block.py
+++ tests/test_text_classifier_block.py
@@ -1,3 +1,4 @@
+from unittest.mock import MagicMock
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
@@ -6,14 +7,69 @@ from ..text_classifier_block import TextClassifier
 
 class TestTextClassifier(NIOBlockTestCase):
 
-    def test_process_signals(self):
-        """Signals pass through block unmodified."""
+    def test_process_sample_data_signal_with_no_fit(self):
+        """Sample data is not predicted if no training data."""
         blk = TextClassifier()
         self.configure_block(blk, {})
+        blk.logger = MagicMock()
         blk.start()
-        blk.process_signals([Signal({"hello": "n.io"})])
+        blk.process_signals([Signal({"sample": "this is sample text"})])
+        blk.stop()
+        self.assert_num_signals_notified(0)
+        self.assertEqual(blk.logger.warning.call_count, 1)
+
+    def test_process_sample_data_signals_with_no_fit(self):
+        """Sample data is not predicted if no training data."""
+        blk = TextClassifier()
+        self.configure_block(blk, {})
+        blk.logger = MagicMock()
+        blk.start()
+        blk.process_signals([Signal({"sample": "this is sample text"}),
+                             Signal({"sample": "this is more text"})])
+        blk.stop()
+        self.assert_num_signals_notified(0)
+        self.assertEqual(blk.logger.warning.call_count, 2)
+
+    def test_process_sample_data_signal(self):
+        """Sample data makes a prediction and gets the target value."""
+        blk = TextClassifier()
+        self.configure_block(blk, {
+            "training_set": [
+                {"data": "this is sample text", "target": "sample"},
+                {"data": "this is more text", "target": "more"},
+            ]
+        })
+        blk.start()
+        blk.process_signals([Signal({"sample": "this is sample text"})])
         blk.stop()
         self.assert_num_signals_notified(1)
-        self.assertDictEqual(
-            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {"hello": "n.io"})
+        self.assertTrue(
+            'target' in self.last_notified[DEFAULT_TERMINAL][0].to_dict())
+        self.assertEqual(
+            self.last_notified[DEFAULT_TERMINAL][0].to_dict()['target'],
+            "sample")
+
+    def test_process_sample_data_signals(self):
+        """Sample data makes a prediction and gets the target value."""
+        blk = TextClassifier()
+        self.configure_block(blk, {
+            "training_set": [
+                {"data": "this is sample text", "target": "sample"},
+                {"data": "this is more text", "target": "more"},
+            ]
+        })
+        blk.start()
+        blk.process_signals([Signal({"sample": "this is sample text"}),
+                             Signal({"sample": "this is more text"})])
+        blk.stop()
+        self.assert_num_signals_notified(2)
+        self.assertTrue(
+            'target' in self.last_notified[DEFAULT_TERMINAL][0].to_dict())
+        self.assertTrue(
+            'target' in self.last_notified[DEFAULT_TERMINAL][1].to_dict())
+        self.assertEqual(
+            self.last_notified[DEFAULT_TERMINAL][0].to_dict()['target'],
+            "sample")
+        self.assertEqual(
+            self.last_notified[DEFAULT_TERMINAL][1].to_dict()['target'],
+            "more")
